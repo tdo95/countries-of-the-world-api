7ea77ef7d0368dd78311e32169d749ec31b513a5
Tee O <tdopress@gmail.com>
2022-09-09 15:44:26 -0400

    added listner to input

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+let searchbox = document.querySelector('input');
+
+searchbox.addEventListener('input', async (e) => {
+    console.log(e.target.value)
+    let text = e.target.value;
+    let res = await fetch(`/api/${text}`);
+    let data = await res.json()
+    console.log(data)
+    
+})
